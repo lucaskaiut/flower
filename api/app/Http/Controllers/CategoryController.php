1a8de685ebea3df46333d86759ba9749ef70cf4f
Lucas Kaiut <lucas.kaiut@gmail.com>
2023-07-23 00:48:41 -0300

    created show category endponit

--- api/app/Http/Controllers/CategoryController.php
+++ api/app/Http/Controllers/CategoryController.php
@@ -33,7 +33,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return new CategoryResource($category);
     }
 
     /**
---
+++ api/tests/Feature/CategoryTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    public function test_create_category()
+    {
+        $payload = [
+            'name' => 'Categoria Teste',
+            'type' => 'in'
+        ];
+
+        $token = User::create([
+            'name' => 'Lucas Kaiut - Test Create Category',
+            'email' => 'lucas.kaiut_test_create_category@gmail.com',
+            'password' => '1815Kaiut!@'
+        ])->createToken('auth')->plainTextToken;
+
+        $response = $this->post(route('category.store'), $payload, [
+            'Authorization' => 'Bearer ' . $token,
+        ]);
+        $response->assertCreated();
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'type',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+
+    public function test_list_categories()
+    {
+        $token = User::create([
+            'name' => 'Lucas Kaiut - Test List Categories',
+            'email' => 'lucas.kaiut_test_list_categories@gmail.com',
+            'password' => '1815Kaiut!@'
+        ])->createToken('auth')->plainTextToken;
+
+        $response = $this->get(route('category.index'), [
+            'Authorization' => 'Bearer ' . $token, 
+        ]);
+
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'current_page',
+            'data' => [
+                [
+                    'id',
+                    'user_id',
+                    'name',
+                    'type',
+                    'created_at',
+                    'updated_at'
+                ],
+            ],
+            'first_page_url',
+            'from',
+            'last_page',
+            'last_page_url',
+            'links' => [
+                [
+                    'url',
+                    'label',
+                    'active',
+                ],
+            ],
+            'next_page_url',
+            'path',
+            'per_page',
+            'prev_page_url',
+            'to',
+            'total',
+        ]);
+    }
+
+    public function test_can_read_a_category() 
+    {
+        $user = User::create([
+            'name' => 'Lucas Kaiut - Test Read Category',
+            'email' => 'lucas.kaiut_test_read_category@gmail.com',
+            'password' => '1815Kaiut!@'
+        ]);
+
+        $category = Category::create(['name' => 'Category Test', 'type' => 'in', 'user_id' => $user->id]);
+
+        $response = $this->get(route('category.show', ['category' => $category->id]), [
+            'Authorization' => 'Bearer ' . $user->createToken('auth')->plainTextToken,
+        ]);
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'type',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+}
