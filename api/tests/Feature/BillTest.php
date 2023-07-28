010974cbc75c7fe54e9a13707bdcf673caef2e64
Lucas Kaiut <lucas.kaiut@gmail.com>
2023-07-27 22:02:09 -0300

    created bill list test

--- api/tests/Feature/BillTest.php
+++ api/tests/Feature/BillTest.php
@@ -37,9 +37,11 @@ class BillTest extends TestCase
 
         $token = $user->createToken('auth')->plainTextToken;
 
-        $response = $this->post(route('bill.store'), $payload, [
-            'Authorization' => 'Bearer ' . $token,
-        ]);
+        $response = $this->post(
+            uri: route('bill.store'), 
+            data: $payload, 
+            headers: ['Authorization' => 'Bearer ' . $token,]
+        );
 
         $response->assertCreated();
         $response->assertJsonStructure([
@@ -55,6 +57,84 @@ class BillTest extends TestCase
                 'amount',
                 'created_at',
                 'updated_at',
+            ],
+        ]);
+    }
+
+    public function test_list_bills()
+    {
+        $user = User::create([
+            'name' => 'Lucas Kaiut - Test Create Category',
+            'email' => 'lucas.kaiut_test_create_category@gmail.com',
+            'password' => '1815Kaiut!@'
+        ]);
+
+        $category = Category::create([
+            'name' => 'Categoria Teste',
+            'type' => 'in',
+            'user_id' => $user->id,
+        ]);
+
+        $payload = [
+            'description' => 'Conta de teste',
+            'reference_date' => Carbon::now()->format('Y-m-d'),
+            'due_at' => Carbon::now()->addMonth()->format('Y-m-d'),
+            'is_paid' => false,
+            'category_id' => $category->id,
+            'amount' => 24.97,
+        ];
+
+        $token = $user->createToken('auth')->plainTextToken;
+
+        $this->post(
+            uri: route('bill.store'), 
+            data: $payload, 
+            headers: ['Authorization' => 'Bearer ' . $token,]
+        );
+
+        $response = $this->get(
+            uri: route('bill.index'), 
+            headers: ['Authorization' => 'Bearer ' . $token,]
+        );
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'data' => [
+                [
+                    'id',
+                    'description',
+                    'reference_date',
+                    'due_at',
+                    'is_paid',
+                    'category_id',
+                    'category',
+                    'user',
+                    'amount',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+            'links' => [
+                'first',
+                'last',
+                'prev',
+                'next',
+            ],
+            'meta' => [
+                'current_page',
+                'from',
+                'last_page',
+                'links' => [
+                    [
+                        'url',
+                        'label',
+                        'active',
+                    ],
+                ],
+                'path',
+                'per_page',
+                'to',
+                'total',
             ],
         ]);
     }
